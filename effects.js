25be5c78600aea97b619458cf426b75e6dfe2bd5
Carsten Østergaard <carsteno@uia.no>
2021-11-14 09:55:06 +0100

    moved stuff effects.js

--- effects.js
+++ effects.js
@@ -1,15 +1,22 @@
 function horRect(){//makes the horizontal rectangles
     for (let i = height; i < height*-1; i = i+distApart){
-        ctx.fillRect(mouse.x - box.w/2, mouse.y + i, box.w, box.h);
+        ctx.fillRect(mouse.x + i, mouse.y + i, box.w, box.h);
     }
 }
 
-
-//random color
-function randomColor() {// stjal na fra jossomjod
-    return 'rgb(' + Math.ceil(Math.random() * 255) + ',' + Math.ceil(Math.random() * 255) + ',' + Math.ceil(Math.random() * 255) + ')';
+//rotates
+let reset = 0;//fix na etterkvart, va ganske stygt
+function rotate(){
+    if (reset === 0){
+        box.w = 2; box.h = 100;
+        reset = 1;
+        console.log("spaceship")
+    }else if(reset === 1){
+        box.w = 100; box.h = 2;
+        reset = 0;
+        console.log("spaceship")
+    }
 }
-
 
 //displays fps
 let lastCalledTime;
@@ -24,4 +31,9 @@ function FPS(){
     lastCalledTime = performance.now();
     fps = Math.round(1/delta);
     document.getElementById('fps').innerHTML = "fps: "+ fps;
+}
+
+//random color
+function randomColor() {// stjal na fra jossomjod
+    return 'rgb(' + Math.ceil(Math.random() * 255) + ',' + Math.ceil(Math.random() * 255) + ',' + Math.ceil(Math.random() * 255) + ')';
 }
