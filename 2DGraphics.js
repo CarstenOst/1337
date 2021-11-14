cc676dbe396e1e8a0a9d81b254b1ea5f0f769ffd
Carsten Østergaard <carsteno@uia.no>
2021-11-14 14:32:14 +0100

    speed 2DGraphics.js

--- 2DGraphics.js
+++ 2DGraphics.js
@@ -5,21 +5,20 @@ canvas.height = window.innerHeight;
 
 // I dunno how to do this so, im making tons of global variables, can't hurt right? RIGHT!? Yeah, I know... bad practise
 let sColor = "#22ff22";//starter color
-let wheelSpeed = 0.1; //speed of mousewheel (lower number, means less scrolling (I know I could switch this around, but why not make it confusing and have way to long comments?))
-let height = -400; //just let it be negative, ait, stop judging
+let wheelSpeed = 0.01; //speed of mousewheel (lower number, means less scrolling (I know I could switch this around, but why not make it confusing and have way to long comments?))
+let height = -1600; //just let it be negative, ait, stop judging
 let distApart = 40;
 //Start size
 let box = {
     w: 100,
     h: 2
 }
-
 function startThisOrSomething(){
     ctx.fillStyle = sColor;
     window.requestAnimationFrame(function update(){//makes it happen pretty much
         ctx.clearRect(0,0,canvas.width, canvas.height);//clears the rectangles
         horRect();
-        FPS()
+        FPS();
         window.requestAnimationFrame(update);
     })
 }
