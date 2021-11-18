f441bffdcc41d42969133fa17feeb081679a074d
Carsten Østergaard <carsteno@uia.no>
2021-11-18 12:08:03 +0100

    balls effects.js

--- effects.js
+++ effects.js
@@ -4,14 +4,44 @@ function horRect(){//makes the horizontal rectangles
     }
 }
 
-function strobeL(){
-    // noinspection JSAnnotator
-    ctx.fillStyle = "#ffffff";
-    void ctx.fillRect(0, 0, 1, canvas.height);
-    void ctx.arc(0, 0, 10, 0, 360 , false);
+function Circle(x, y, r, c){
+    //this is now a ball
+    this.x = c;
+    this.y = y;
+    this.r = r;
+    this.c = randomColor();
 
+    this.dx = (Math.random()*4)+1;
+    this.dy = (Math.random()*4)+1;
 
+    this.draw = function (){
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        ctx.fill();
+    }
+    this.animate = function (){
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
 }
+const balls = [];
+for (let i = 0; i<20; i++){
+    let r = Math.floor(Math.random()*30)+15;
+    let x = Math.random()* (canvas.width-r*2)+r;
+    let y = Math.random()* (canvas.height-r*2)+r;
+    let c = randomColor();
+    balls.push(new Circle(x, y, r, c))
+}
+function uptoDate(){
+    for (let i = 0; i < balls.length; i++){
+        balls[i].animate();
+    }
+}
+
+3
 
 //rotates
 let reset = 0;//fix na etterkvart, va ganske stygt
