4245f6ab305a210b8e43baad81cc77da3dfb1e80
Carsten Østergaard <carsteno@uia.no>
2021-12-03 10:55:30 +0100

    Cut off my balls :(

--- effects.js
+++ effects.js
@@ -4,44 +4,12 @@ function horRect(){//makes the horizontal rectangles
     }
 }
 
-function Circle(x, y, r, c){
-    //this is now a ball
-    this.x = c;
-    this.y = y;
-    this.r = r;
-    this.c = randomColor();
 
-    this.dx = (Math.random()*4)+1;
-    this.dy = (Math.random()*4)+1;
-
-    this.draw = function (){
-        ctx.beginPath();
-        ctx.fillStyle = this.c;
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
-        ctx.fill();
-    }
-    this.animate = function (){
-        this.x += this.dx;
-        this.y += this.dy;
-
-        this.draw();
-    }
-}
-const balls = [];
-for (let i = 0; i<20; i++){
-    let r = Math.floor(Math.random()*30)+15;
-    let x = Math.random()* (canvas.width-r*2)+r;
-    let y = Math.random()* (canvas.height-r*2)+r;
-    let c = randomColor();
-    balls.push(new Circle(x, y, r, c))
-}
 function uptoDate(){
-    for (let i = 0; i < balls.length; i++){
-        balls[i].animate();
-    }
+    ctx1.fillStyle = fade(255, 0, 10);
+    ctx1.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-3
 
 //rotates
 let reset = 0;//fix na etterkvart, va ganske stygt
