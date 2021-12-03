d789d3c15533a98afc9050445b91b79008d539a0
Carsten Østergaard <carsteno@uia.no>
2021-12-03 10:53:09 +0100

    Added another canvas 2DGraphics.js, i dunno wtf im actually doing

--- 2DGraphics.js
+++ 2DGraphics.js
@@ -3,6 +3,12 @@ const ctx = canvas.getContext("2d", { alpha: true });
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const canvas1 = document.getElementById("canvas2");
+const ctx1 = canvas1.getContext("2d", { alpha: true }); //speedy if false / however, wont work as well
+canvas1.width = window.innerWidth;
+canvas1.height = window.innerHeight;
+
+
 // I dunno how to do this so, im making tons of global variables, can't hurt right? RIGHT!? Yeah, I know... bad practise, or is it?
 let sColor = "#22ff22";//starter color
 let wheelSpeed = 0.01; //speed of mousewheel (lower number, means less scrolling (I know I could switch this around, but why not make it confusing and have way to long comments?))
@@ -19,6 +25,7 @@ function startThisOrSomething(){
     ctx.fillStyle = sColor;
     window.requestAnimationFrame(function update(){//makes it happen pretty much
         ctx.clearRect(0,0,canvas.width, canvas.height);//clears the rectangles
+        ctx1.clearRect(0,0,canvas.width, canvas.height);//clears the rectangles
 
         if (toglStrobe === 1){
             uptoDate();
